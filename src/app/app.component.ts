76bf787af388348dde51bfbb9405493326854f0c
bettoarcaya <betoarcaya@gmail.com>
2018-09-20 11:36:40 -0400

    instalando chartjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  chart : any = null;
+  chartRealtime : any = null;
+
+
+  ngOnInit() : void {
+
+    this.chart = new Chart('realtime', {
+      type: 'line',
+      data: {
+       labels: [],
+       datasets: [
+         {
+        label: 'Data',
+        fill: false,
+        data: [],
+        backgroundColor: '#168ede',
+        borderColor: '#168ede'
+         }
+       ]
+        },
+        options: {
+       tooltips: {
+        enabled: false
+       },
+       legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+         fontColor: 'white'
+        }
+       },
+       scales: {
+         yAxes: [{
+          ticks: {
+           fontColor: "white"
+          }
+         }],
+         xAxes: [{
+        ticks: {
+         fontColor: "white",
+         beginAtZero: true
+        }
+         }]
+       }
+        }
+     });
+
+
+  }
+
+
 }
