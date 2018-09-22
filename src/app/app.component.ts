944640a46f9fb24505b2ce94ce8d8a639dbc09ae
bettoarcaya <betoarcaya@gmail.com>
2018-09-22 10:36:41 -0400

    creando la configuracion del chartjs para mostrar la grafica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,52 +10,54 @@ export class AppComponent {
   title = 'app';
   chart : any = null;
   chartRealtime : any = null;
-
+  dataFirst = {
+    label: "",
+            data: [ 100, 200, 300 ],
+            lineTension: 0.3,
+            fill: false,
+            borderColor: 'red',
+            backgroundColor: 'transparent',
+            pointBorderColor: 'red',
+            pointBackgroundColor: 'lightgreen',
+            pointRadius: 5,
+            pointHoverRadius: 15,
+            pointHitRadius: 30,
+            pointBorderWidth: 2,
+            pointStyle: 'rect'
+  };
+  speedData = {
+    labels: [ 
+      "100", 
+      "200", 
+      "300", 
+      "400", 
+      "500", 
+      "600", 
+      "700",
+      "800",
+      "900",
+      "1000"
+    ],
+    datasets: [this.dataFirst]     
+  };
+  chartOptions = {
+    legend: {
+      display: true,
+      position: 'top',
+      labels: {
+        boxWidth: 80,
+        fontColor: 'black'
+      }
+    }
+  };
 
   ngOnInit() : void {
-
+    
     this.chart = new Chart('realtime', {
       type: 'line',
-      data: {
-       labels: [],
-       datasets: [
-         {
-        label: 'Data',
-        fill: false,
-        data: [],
-        backgroundColor: '#168ede',
-        borderColor: '#168ede'
-         }
-       ]
-        },
-        options: {
-       tooltips: {
-        enabled: false
-       },
-       legend: {
-        display: true,
-        position: 'bottom',
-        labels: {
-         fontColor: 'white'
-        }
-       },
-       scales: {
-         yAxes: [{
-          ticks: {
-           fontColor: "white"
-          }
-         }],
-         xAxes: [{
-        ticks: {
-         fontColor: "white",
-         beginAtZero: true
-        }
-         }]
-       }
-        }
-     });
-
-
+      data: this.speedData,
+      options: this.chartOptions
+      });
   }
 
 
